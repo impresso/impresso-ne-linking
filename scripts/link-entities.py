74eb76730f330bb4456195477cab777552593667
Matteo Romanello <matteo.romanello@gmail.com>
2019-12-20 15:50:15 +0100

    tweaked link-entities.py to work with single machine dask cluster

--- scripts/link-entities.py
+++ scripts/link-entities.py
@@ -1,7 +1,7 @@
 """CLI script to disambiguate entities with Aida-light.
 
 Usage:
-    link-entities.py --input-dir=<inp> --output-dir=<out> --files-range=<r> [--gateway-port=<p>]
+    link-entities.py --input-dir=<inp> --output-dir=<out> --files-range=<r> [--gateway-port=<p> --scheduler=<ds>]
 
 Options:
 --input-dir=<inp>
@@ -17,6 +17,7 @@ from docopt import docopt
 from dask.distributed import Client
 from dask import bag as db
 from py4j.java_gateway import JavaGateway, GatewayParameters
+from dask.distributed import Client
 
 # from impresso_images.data_utils import fixed_s3fs_glob
 
@@ -24,28 +25,19 @@ from py4j.java_gateway import JavaGateway, GatewayParameters
 def aida_disambiguate_documents(documents, gateway_port):
 
     # do py4j business
-    gateway = JavaGateway(
-        gateway_parameters=GatewayParameters(port=gateway_port)
-    )
+    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=gateway_port))
     aida_server = gateway.entry_point.getAida()
 
     output = []
+    print(f"Linking partition with {len(documents)} documents [{', '.join([doc['id'] for doc in documents])}]")
 
     for document in documents:
 
         prepared_input = document['input']
         mentions = gateway.entry_point.findExtractedMentions(prepared_input)
-        annotations = aida_server.disambiguate(
-            prepared_input,
-            mentions,
-            "fullSettings"
-        )
+        annotations = aida_server.disambiguate(prepared_input, mentions, "fullSettings")
 
-        output_doc = {
-            "id": document['id'],
-            "sys_id": "aidalight-fullSettings",
-            "cdt": strftime("%Y-%m-%d %H:%M:%S")
-        }
+        output_doc = {"id": document['id'], "sys_id": "aidalight-fullSettings", "cdt": strftime("%Y-%m-%d %H:%M:%S")}
         linked_entities = []
 
         annotations = [
@@ -54,7 +46,7 @@ def aida_disambiguate_documents(documents, gateway_port):
                 "entity_id": annotations.get(ann).getName(),
                 "normalized_name": annotations.get(ann).getNMEnormalizedName(),
                 "char_offset": ann.getCharOffset(),
-                "mention_entity_similarity": annotations.get(ann).getMentionEntitySimilarity()
+                "mention_entity_similarity": annotations.get(ann).getMentionEntitySimilarity(),
             }
             for ann in annotations.keySet()
         ]
@@ -63,11 +55,7 @@ def aida_disambiguate_documents(documents, gateway_port):
 
             # since some character offsets are broken we rely
             # on the surface to realign mention and disambiguations
-            matching_entity = [
-                annotation
-                for annotation in annotations
-                if annotation['surface'] == mention['surface']
-            ]
+            matching_entity = [annotation for annotation in annotations if annotation['surface'] == mention['surface']]
 
             if len(matching_entity) == 0:
                 continue
@@ -84,7 +72,7 @@ def aida_disambiguate_documents(documents, gateway_port):
                     "entity_id": entity_id,
                     "entity_link": f"http://en.wikipedia.org/wiki/{entity_id}",
                     "normalized_name": matching_entity['normalized_name'],
-                    "mention_entity_similarity": matching_entity["mention_entity_similarity"]
+                    "mention_entity_similarity": matching_entity["mention_entity_similarity"],
                 }
             )
         output_doc['ne_links'] = linked_entities
@@ -97,10 +85,17 @@ def main():
     arguments = docopt(__doc__)
     local_input_dir = arguments["--input-dir"]
     local_output_dir = arguments["--output-dir"]
+    dask_scheduler_port = arguments["--scheduler"]
 
     port = arguments["--gateway-port"]
     gateway_port = 25333 if not port else int(port)
     print(f"Java Gateway will be using port {gateway_port}")
+
+    if scheduler:
+        dask_client = Client(scheduler)
+    else:
+        dask_client = Client()
+    print(f"{client}")
 
     range_start, range_end = arguments['--files-range'].split('-')
     range_start = int(range_start)
@@ -109,14 +104,10 @@ def main():
     aida_input_files = [
         os.path.join(local_input_dir, file)
         for file in os.listdir(local_input_dir)
-        if int(file.replace(".bz2", "")) >= range_start and
-        int(file.replace(".bz2", "")) < range_end
+        if int(file.replace(".bz2", "")) >= range_start and int(file.replace(".bz2", "")) < range_end
     ]
 
-    aida_output_files = [
-        os.path.join(local_output_dir, os.path.basename(file))
-        for file in aida_input_files
-    ]
+    aida_output_files = [os.path.join(local_output_dir, os.path.basename(file)) for file in aida_input_files]
 
     print(f"Number of input files: {len(aida_input_files)}")
     print(f"Number of input files: {len(aida_output_files)}")
@@ -128,14 +119,15 @@ def main():
     dask_client = Client(processes=False, n_workers=8)
     print(f"{dask_client}")
 
-    aida_input_bag = db.read_text(
-        aida_input_files,
-        # storage_options=IMPRESSO_STORAGEOPT
-    ).map(json.loads).map_partitions(
-        aida_disambiguate_documents,
-        gateway_port
-    ).map(json.dumps).to_textfiles(
-        aida_output_files
+    aida_input_bag = (
+        db.read_text(
+            aida_input_files,
+            # storage_options=IMPRESSO_STORAGEOPT
+        )
+        .map(json.loads)
+        .map_partitions(aida_disambiguate_documents, gateway_port)
+        .map(json.dumps)
+        .to_textfiles(aida_output_files)
     )
 
     print(aida_input_bag)
